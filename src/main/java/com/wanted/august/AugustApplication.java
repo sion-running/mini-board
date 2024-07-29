e1c83284b0aad21925e9f9fae63654b07c47a7b0
sion-running <staythismoment.sion@gmail.com>
2024-07-29 09:50:10 +0900

    데이터 생성,수정,삭제 관련 공통 처리를 위한 entity 추가

--- src/main/java/com/wanted/august/AugustApplication.java
+++ src/main/java/com/wanted/august/AugustApplication.java
@@ -2,7 +2,9 @@ package com.wanted.august;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@EnableJpaAuditing
 @SpringBootApplication
 public class AugustApplication {
 
---
+++ src/main/java/com/wanted/august/entity/BaseEntity.java
@@ -0,0 +1,32 @@
+package com.wanted.august.entity;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import jakarta.persistence.Column;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.MappedSuperclass;
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
+
+@JsonInclude(JsonInclude.Include.NON_NULL)
+@Getter
+@Setter
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+public class BaseEntity {
+    @CreatedDate
+    @Column(updatable = false, columnDefinition = "datetime default CURRENT_TIMESTAMP NOT NULL COMMENT '생성일자'")
+    private LocalDateTime createdAt;
+
+    @LastModifiedDate
+    @Column(columnDefinition = "datetime default CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP NOT NULL COMMENT '수정일자'")
+    private LocalDateTime updatedAt;
+
+    @Column(columnDefinition = "bit default false NOT NULL COMMENT '삭제여부'")
+    private Boolean isDeleted;
+
+}
