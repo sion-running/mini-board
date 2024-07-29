7545b986fd292ceeeffff4818e77afeffd06b354
sion-running <staythismoment.sion@gmail.com>
2024-07-29 09:24:33 +0900

    initial commit

---
+++ src/main/java/com/wanted/august/AugustApplication.java
@@ -0,0 +1,13 @@
+package com.wanted.august;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AugustApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AugustApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/wanted/august/AugustApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wanted.august;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AugustApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
